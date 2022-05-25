ea04904f50644afa48cd809f9ad820897464ec73
Sergey <moya1pochta@gmail.com>
2022-05-25 13:23:05 +0300

    Files combine bug fixed

--- Files/filescombine.py
+++ Files/filescombine.py
@@ -10,20 +10,18 @@ def sort_text(list_to_sort):
     text_dict = {}
     for file in list_to_sort:
         text_dict[file + "\n"] = get_text(file)
-    text_dict_to_sort = {}
-    for name, text in text_dict.items():
-        text_dict_to_sort[str(len(text)) + "\n"] = {name: text}
-    return sorted(text_dict_to_sort.items())
+    return sorted(text_dict.items(), key=lambda x: len(x[1]))
 
 def combine_text(text_to_write):
     with open(output_name, 'w') as out_f:
         for i in range(len(text_to_write)):
-            out_f.write(str(*text_to_write[i][1].keys()))
-            out_f.write(str(text_to_write[i][0]))
-            for text in text_to_write[i][1].values():
+            out_f.write(text_to_write[i][0])
+            out_f.write(str(len(text_to_write[i][1])) + "\n")
+            for text in text_to_write[i][1]:
                 for line in text:
                     out_f.write(line)
             out_f.write("\n")
             
 if __name__ == '__main__':
     combine_text(sort_text(files_list))
+
