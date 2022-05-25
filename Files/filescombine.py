fe57160ea8b69c66b51b8f9f23166faf7a05e6ad
Sergey <moya1pochta@gmail.com>
2022-05-25 22:29:15 +0300

    Fixed masters notes

--- Files/filescombine.py
+++ Files/filescombine.py
@@ -1,5 +1,6 @@
-files_list = ['1.txt', '2.txt']
-output_name = input('Введите название выходного файла:') + '.txt'
+def main():
+    files_list = ['1.txt', '2.txt']
+    write_text_to_file(sort_text(files_list))
 
 def get_text(file_name):
     with open(file_name, 'r') as read_file:
@@ -12,16 +13,16 @@ def sort_text(list_to_sort):
         text_dict[file + "\n"] = get_text(file)
     return sorted(text_dict.items(), key=lambda x: len(x[1]))
 
-def combine_text(text_to_write):
-    with open(output_name, 'w') as out_f:
-        for i in range(len(text_to_write)):
-            out_f.write(text_to_write[i][0])
-            out_f.write(str(len(text_to_write[i][1])) + "\n")
-            for text in text_to_write[i][1]:
-                for line in text:
+def write_text_to_file(text_to_write):
+    with open(input('Введите название выходного файла:') + '.txt', 'w') as out_f:
+        for text in text_to_write:
+            out_f.write(text[0])
+            out_f.write(str(len(text[1])) + "\n")
+            for lines in text[1]:
+                for line in lines:
                     out_f.write(line)
             out_f.write("\n")
             
 if __name__ == '__main__':
-    combine_text(sort_text(files_list))
-
+    main()
+    
